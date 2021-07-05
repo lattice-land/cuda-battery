450dbb4056112cbc02b91fa010dd3f3bb011a6b4
Pierre Talbot <pierre.talbot@uni.lu>
2021-07-05 12:47:51 +0200

    Put tuple structure in the battery namespace.

--- include/tuple.hpp
+++ include/tuple.hpp
@@ -5,9 +5,34 @@
 
 #ifdef __NVCC__
   #include <cuda/std/tuple>
-  using namespace cuda;
 #else
   #include <tuple>
 #endif
+
+namespace battery {
+#ifdef __NVCC__
+  using cuda::std::tuple;
+  using cuda::std::make_tuple;
+  using cuda::std::tie;
+  using cuda::std::forward_as_tuple;
+  using cuda::std::tuple_cat;
+  using cuda::std::get;
+  using cuda::std::tuple_size;
+  using cuda::std::tuple_element;
+  using cuda::std::uses_allocator;
+  using cuda::std::ignore;
+#else
+  using std::tuple;
+  using std::make_tuple;
+  using std::tie;
+  using std::forward_as_tuple;
+  using std::tuple_cat;
+  using std::get;
+  using std::tuple_size;
+  using std::tuple_element;
+  using std::uses_allocator;
+  using std::ignore;
+#endif
+}
 
 #endif
