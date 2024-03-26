6fff59de838c1acc466171837f11838554afc53a
Pierre Talbot <pierre.talbot@uni.lu>
2024-03-26 14:17:38 +0100

    Add a version of sort which gives the indexes of the elements instead of the elements themselves.

--- include/battery/algorithm.hpp
+++ include/battery/algorithm.hpp
@@ -17,7 +17,7 @@ CUDA int partition(Seq& seq, int low, int high, Compare comp) {
 
   for (int j = low; j <= high - 1; j++) {
     // If current element is smaller than the pivot
-    if (comp(seq[j], seq[high])) {
+    if (comp(j, high)) {
       i++; // Increment index of smaller element
       ::battery::swap(seq[i], seq[j]);
     }
@@ -54,10 +54,18 @@ CUDA void quickSort(Seq& seq, Compare comp) {
 }// namespace impl
 
 /** Sort the sequence `seq` in-place.
- * The underlying algorithm is quicksort.
+ * The underlying algorithm is an iterative version of quicksort.
  * * `comp(a, b)` returns `true` whenever a < b, and false otherwise. */
 template <class Seq, class Compare>
 CUDA NI void sort(Seq& seq, Compare comp) {
+  assert(seq.size() < limits<int>::top());
+  impl::quickSort(seq, [&](int i, int j) { return comp(seq[i], seq[j]); });
+}
+
+/** Similar to `sort`, but the comparison function is takes indexes instead of elements themselves.
+ * * `comp(i, j)` returns `true` whenever seq[i] < seq[j], and false otherwise. */
+template <class Seq, class Compare>
+CUDA NI void sorti(Seq& seq, Compare comp) {
   assert(seq.size() < limits<int>::top());
   impl::quickSort(seq, comp);
 }
